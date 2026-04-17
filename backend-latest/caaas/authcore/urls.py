6782175abb01bf1601b8505ca7b3e5eca02a364c
Aryaaravindk <khadedarya@gmail.com>
2026-04-17 06:11:40 +0530

    Added dashboard analytics apis

--- backend-latest/caaas/authcore/urls.py
+++ backend-latest/caaas/authcore/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import VerifyRequestView, RegisterView, LoginView,TrainModelView
+from .views import UserSummaryView, UserAnalyticsView, UserLogsView
 
 urlpatterns = [
     path("auth/register", RegisterView.as_view()),
     path("auth/login", LoginView.as_view()),
     path("auth/verify", VerifyRequestView.as_view()),
     path("train/", TrainModelView.as_view()),
+
+# dashboard apis
+    path("user/summary/", UserSummaryView.as_view()),
+    path("user/analytics/", UserAnalyticsView.as_view()),
+    path("user/logs/", UserLogsView.as_view()),
 
 ]
